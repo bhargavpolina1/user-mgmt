a88dbfd06e080c142ea43203e9a12022d290fbbe
pgbpolina <bhargavpolina1996@gmail.com>
2022-07-01 19:59:43 +0530

    Update naming conventions

--- src/components/addUser.js
+++ src/components/addUser.js
@@ -254,16 +254,16 @@ obtainedJson = "";
       reader.onload = (e) => {
       // Use reader.result
       var csv = reader.result;
-      console.log(csv,csv.length)
+      console.log(csv)
       var lines = csv.split("\n");
       console.log(lines)
       var result = [];
-      var headers=lines[0].split(",");
+      var columnNames=lines[0].split(",");
       for(var i=1;i<lines.length;i++){
         var obj = {};
-        var currentline=lines[i].split(",");
-        for(var j=0;j<headers.length;j++){
-          obj[headers[j]] = currentline[j];
+        var userDetails=lines[i].split(",");
+        for(var j=0;j<columnNames.length;j++){
+          obj[columnNames[j]] = userDetails[j];
         }
         result.push(obj);
         }
